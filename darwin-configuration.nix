dd1596ce551f3702b17e07fd9e953e692661d78f
Matthew Ryall <matthew@mexico86.co.uk>
2021-01-06 22:46:33 +0000

    More Emacs packages

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -21,6 +21,7 @@
       config = ~/.emacs;
       extraEmacsPackages = epkgs: [
         epkgs.ace-window
+        epkgs.add-node-modules-path
         epkgs.ag
         epkgs.base16-theme
         epkgs.company
@@ -31,9 +32,12 @@
         epkgs.docker
         epkgs.flx
         epkgs.flycheck
+        epkgs.flycheck-kotlin
         epkgs.forge
         epkgs.ivy
+        epkgs.js2-mode
         epkgs.kotlin-mode
+        epkgs.lsp-ivy
         epkgs.lsp-java
         epkgs.lsp-mode
         epkgs.magit
@@ -42,13 +46,16 @@
         epkgs.org-roam
         epkgs.org-roam-server
         epkgs.paredit
+        epkgs.prettier-js
         epkgs.projectile
         epkgs.racket-mode
         epkgs.smex
         epkgs.spaceline
+        epkgs.typescript-mode
         epkgs.use-package
         epkgs.use-package-ensure-system-package
         epkgs.which-key
+        epkgs.yaml-mode
       ];
     })
   ];
