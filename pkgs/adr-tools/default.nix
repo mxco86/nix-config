aa5015e3a05f5c447a5d03ee38d376f7f0bace93
Matthew Ryall <matthew@mexico86.co.uk>
2021-04-05 09:15:17 +0100

    Fix ADR Tools package NixOS build

--- pkgs/adr-tools/default.nix
+++ pkgs/adr-tools/default.nix
@@ -3,11 +3,20 @@ with (import <nixpkgs> { });
 stdenv.mkDerivation rec {
   pname = "adr-tools";
   version = "3.0.0";
+
   src = fetchurl {
     url = "https://github.com/npryce/adr-tools/archive/${version}.tar.gz";
     sha256 = "9490f31a457c253c4113313ed6352efcbf8f924970a309a08488833b9c325d7c";
   };
-  buildInputs = [ bashInteractive ];
+
+  buildInputs = [ getopt bashInteractive ];
+
+  patches = [ ./fix-paths.patch ];
+  postPatch = ''
+    patchShebangs src/*
+    patchShebangs approve
+  '';
+
   installPhase = ''
     mkdir -p $out/bin
     cp src/* $out/bin
