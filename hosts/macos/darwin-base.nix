e523f90206b8b260667410d3f05039847e7d7b6c
Matthew Ryall <matthew@mexico86.co.uk>
2021-04-06 09:29:42 +0100

    Add macOS keyboard shortcuts

--- hosts/macos/darwin-base.nix
+++ hosts/macos/darwin-base.nix
@@ -53,9 +53,10 @@
     skhd = {
       enable = true;
       skhdConfig = ''
-        ralt - 2: open ~/Applications/Nix\ Apps/Emacs.app
         ralt - 1: open ~/.nix-profile/Applications/kitty.app
+        ralt - 2: open ~/Applications/Nix\ Apps/Emacs.app
         ralt - 3: open /Applications/Firefox.app
+        ralt - 4: open ~/.nix-profile/Applications/Slack.app
       '';
     };
   };
