78c3e6dfa752a2af7cef64853ae7e3dca6fc41a5
Matthew Ryall <matthew@mexico86.co.uk>
2021-09-19 19:30:49 +0100

    Add Python development environment

---
+++ env/python.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> { };
+let
+  python3Env = pkgs.python3.withPackages (ps: with ps; [
+    black
+    pip
+    pylint
+    virtualenv
+  ]);
+in
+pkgs.mkShell rec {
+  buildInputs = [
+    python3Env
+  ];
+
+  shellHook = ''
+    virtualenv --no-wheel --no-setuptools venv
+    source ./venv/bin/activate
+    unset PYTHONPATH
+  '';
+}
