2007f22185ba2d6eb2e5f1aa1947d5e041bc9d2b
Matthew Ryall <matthew@mexico86.co.uk>
2021-12-31 12:00:23 +0000

    Remove nix package config from NixOS-specific config

--- hosts/nixos/nixos-base.nix
+++ hosts/nixos/nixos-base.nix
@@ -4,13 +4,6 @@
 
   imports = [ ../../cachix.nix ];
 
-  nix = {
-    package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
   programs = {
     gnupg.agent = {
       enable = true;
