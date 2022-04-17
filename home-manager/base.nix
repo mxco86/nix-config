e74450de8dcee9eba99d9f514962c2d009715fcc
Matthew Ryall <matthew@mexico86.co.uk>
2022-04-17 10:33:41 +0100

    Move Firefox CSS to OS-specific config

--- home-manager/base.nix
+++ home-manager/base.nix
@@ -111,13 +111,6 @@
             "browser.urlbar.placeholderName" = "DuckDuckGo";
             "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
           };
-          userChrome = ''
-            #TabsToolbar { visibility: collapse !important; }
-
-            #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
-                display:none;
-            }
-          '';
         };
       };
 
--- home-manager/macos/base.nix
+++ home-manager/macos/base.nix
@@ -19,6 +19,17 @@
     };
     firefox = {
       package = pkgs.runCommand "firefox-0.0.0" { } "mkdir $out";
+      profiles = {
+        mryall = {
+          userChrome = ''
+            #TabsToolbar { visibility: collapse !important; }
+
+            #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
+                display:none;
+            }
+          '';
+        };
+      };
     };
   };
 }
