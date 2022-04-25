5f45e4b81ff8aadde36e0301c6b4d13c1c07b726
Matthew Ryall <matthew@mexico86.co.uk>
2022-04-25 20:47:09 +0100

    Workstation network config

--- hosts/nixos/workstation/default.nix
+++ hosts/nixos/workstation/default.nix
@@ -12,14 +12,22 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "rossi";
-  networking.domain = "mexico86.co.uk";
+  networking = {
+    hostName = "rossi";
+    domain = "mexico86.co.uk";
+    useDHCP = false;
+    interfaces.enp4s0.useDHCP = true;
+    networkmanager = {
+      enable = true;
+      enableStrongSwan = true;
+    };
+  };
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp4s0.useDHCP = true;
+  environment = {
+    etc."ipsec.secrets".text = ''
+      include ipsec.d/ipsec.nm-l2tp.secrets
+    '';
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
