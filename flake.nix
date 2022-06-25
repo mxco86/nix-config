1745b9347e131bdbd5d278aaf29a580ad9200044
Matthew Ryall <matthew@mexico86.co.uk>
2022-06-25 08:03:12 +0100

    Tidy up system flake

--- flake.nix
+++ flake.nix
@@ -59,152 +59,117 @@
             ];
           in
           pkgs.mkShell { nativeBuildInputs = pkglist; };
-
       };
-
-      nixosConfigurations.sanchez =
-        let
-          overlays = [ inputs.emacs-overlay.overlay ];
-        in
-        nixpkgs.lib.nixosSystem {
+      nixosConfigurations = {
+        sanchez = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          modules = [
-            ./hosts/nixos/thinkpad/default.nix
-            { nixpkgs.overlays = overlays; }
-          ];
-          specialArgs = {
-            inherit inputs;
-          };
+          modules = [ ./hosts/nixos/thinkpad/default.nix { nixpkgs.overlays = [ inputs.emacs-overlay.overlay ]; } ];
+          specialArgs = { inherit inputs; };
         };
-
-      nixosConfigurations.rossi =
-        let
-          overlays = [ inputs.emacs-overlay.overlay ];
-        in
-        nixpkgs.lib.nixosSystem {
+        rossi = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          modules = [
-            ./hosts/nixos/workstation/default.nix
-            { nixpkgs.overlays = overlays; }
-          ];
-          specialArgs = {
-            inherit inputs;
-          };
+          modules = [ ./hosts/nixos/workstation/default.nix { nixpkgs.overlays = [ inputs.emacs-overlay.overlay ]; } ];
+          specialArgs = { inherit inputs; };
         };
-
+      };
       darwinConfigurations = {
-        socrates =
-          let
-            system = "x86_64-darwin";
-            overlays = [ inputs.emacs-overlay.overlay kittyOverlay ];
-          in
-          darwin.lib.darwinSystem
-            {
-              inherit system inputs;
-              modules = [
-                ./hosts/macos/macbook.nix
-                { nixpkgs.overlays = overlays; }
-              ];
-            };
-        careca =
-          let
-            system = "aarch64-darwin";
-            overlays = [ inputs.emacs-overlay.overlay kittyOverlay ];
-          in
-          darwin.lib.darwinSystem
-            {
-              inherit system inputs;
-              modules = [
-                ./hosts/macos/macbook-work.nix
-                { nixpkgs.overlays = overlays; }
-              ];
-              specialArgs = {
-                x86pkgs = import nixpkgs { system = "x86_64-darwin"; };
-              };
-            };
-      };
-
-      homeConfigurations =
-        let
-          systemNixOS = "x86_64-linux";
-          systemMacOS = "x86_64-darwin";
-          systemMacOSM1 = "aarch64-darwin";
-        in
-        {
-          mryallNixOSThinkpad = inputs.home-manager.lib.homeManagerConfiguration {
-            system = systemNixOS;
-            homeDirectory = "/home/mryall";
-            username = "mryall";
-            configuration.imports = [
-              ./home-manager/nixos/thinkpad-home.nix
-              {
-                nixpkgs.config.packageOverrides = pkgs: {
-                  nur = import inputs.nur {
-                    inherit pkgs;
-                    nurpkgs = import nixpkgs { system = systemNixOS; };
-                  };
-                };
-              }
-            ];
-          };
-          mryallNixOSWorkstation = inputs.home-manager.lib.homeManagerConfiguration {
-            system = systemNixOS;
-            homeDirectory = "/home/mryall";
-            username = "mryall";
-            configuration.imports = [
-              ./home-manager/nixos/workstation-home.nix
-              {
-                nixpkgs.config.packageOverrides = pkgs: {
-                  nur = import inputs.nur {
-                    inherit pkgs;
-                    nurpkgs = import nixpkgs { system = systemNixOS; };
-                  };
-                };
-              }
-            ];
-          };
-          mryallMacOS = inputs.home-manager.lib.homeManagerConfiguration {
-            system = systemMacOS;
-            homeDirectory = "/Users/mryall";
-            username = "mryall";
-            pkgs = import inputs.nixpkgs {
-              overlays = [ kittyOverlay ];
-              system = systemMacOS;
-            };
-            configuration.imports = [
-              ./home-manager/macos/home.nix
-              {
-                nixpkgs.config.packageOverrides = pkgs: {
-                  nur = import inputs.nur {
-                    inherit pkgs;
-                    nurpkgs = import nixpkgs { system = systemMacOS; };
-                  };
-                };
-              }
-            ];
-          };
-          mryallMacOSWork = inputs.home-manager.lib.homeManagerConfiguration {
-            system = systemMacOSM1;
-            homeDirectory = "/Users/matthew.ryall";
-            username = "matthew.ryall";
-            pkgs = import inputs.nixpkgs {
-              system = systemMacOSM1;
-            };
-            extraSpecialArgs = {
-              x86pkgs = import nixpkgs { system = "x86_64-darwin"; };
-            };
-            configuration. imports = [
-              ./home-manager/macos/work-home.nix
-              {
-                nixpkgs. config. packageOverrides = pkgs: {
-                  nur = import inputs.nur {
-                    inherit pkgs;
-                    nurpkgs = import nixpkgs { system = systemMacOSM1; };
-                  };
-                };
-              }
-            ];
+        socrates = darwin.lib.darwinSystem {
+          inherit inputs;
+          system = "x86_64-darwin";
+          modules = [ ./hosts/macos/macbook.nix { nixpkgs.overlays = [ inputs.emacs-overlay.overlay kittyOverlay ]; } ];
+          specialArgs = {
+            x86pkgs = import nixpkgs { system = "x86_64-darwin"; overlays = [ inputs.emacs-overlay.overlay kittyOverlay ]; };
           };
         };
+        careca = darwin.lib.darwinSystem {
+          inherit inputs;
+          system = "aarch64-darwin";
+          modules = [ ./hosts/macos/macbook-work.nix { nixpkgs.overlays = [ inputs.emacs-overlay.overlay kittyOverlay ]; } ];
+          specialArgs = {
+            x86pkgs = import nixpkgs { system = "x86_64-darwin"; };
+          };
+        };
+      };
+      homeConfigurations = {
+        mryallNixOSThinkpad = inputs.home-manager.lib.homeManagerConfiguration {
+          system = "x86_64-linux";
+          homeDirectory = "/home/mryall";
+          username = "mryall";
+          configuration.imports = [
+            ./home-manager/nixos/thinkpad-home.nix
+            {
+              nixpkgs.config.packageOverrides = pkgs: {
+                nur = import inputs.nur {
+                  inherit pkgs;
+                  nurpkgs = import nixpkgs { system = "x86_64-linux"; };
+                };
+              };
+            }
+          ];
+        };
+        mryallNixOSWorkstation = inputs.home-manager.lib.homeManagerConfiguration {
+          system = "x86_64-linux";
+          homeDirectory = "/home/mryall";
+          username = "mryall";
+          configuration.imports = [
+            ./home-manager/nixos/workstation-home.nix
+            {
+              nixpkgs.config.packageOverrides = pkgs: {
+                nur = import inputs.nur {
+                  inherit pkgs;
+                  nurpkgs = import nixpkgs { system = "x86_64-linux"; };
+                };
+              };
+            }
+          ];
+        };
+        mryallMacOS = inputs.home-manager.lib.homeManagerConfiguration {
+          system = "x86_64-darwin";
+          homeDirectory = "/Users/mryall";
+          username = "mryall";
+          pkgs = import inputs.nixpkgs {
+            system = "x86_64-darwin";
+          };
+          extraSpecialArgs = {
+            x86pkgs = import nixpkgs {
+              system = "x86_64-darwin";
+              overlays = [ kittyOverlay ];
+            };
+          };
+          configuration.imports = [
+            ./home-manager/macos/home.nix
+            {
+              nixpkgs.config.packageOverrides = pkgs: {
+                nur = import inputs.nur {
+                  inherit pkgs;
+                  nurpkgs = import nixpkgs { system = "x86_64-darwin"; };
+                };
+              };
+            }
+          ];
+        };
+        mryallMacOSWork = inputs.home-manager.lib.homeManagerConfiguration {
+          system = "aarch64-darwin";
+          homeDirectory = "/Users/matthew.ryall";
+          username = "matthew.ryall";
+          pkgs = import inputs.nixpkgs {
+            system = "aarch64-darwin";
+          };
+          extraSpecialArgs = {
+            x86pkgs = import nixpkgs { system = "x86_64-darwin"; };
+          };
+          configuration. imports = [
+            ./home-manager/macos/work-home.nix
+            {
+              nixpkgs.config.packageOverrides = pkgs: {
+                nur = import inputs.nur {
+                  inherit pkgs;
+                  nurpkgs = import nixpkgs { system = "aarch64-darwin"; };
+                };
+              };
+            }
+          ];
+        };
+      };
     };
 }
