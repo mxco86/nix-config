0284c90f57d23497ad0bfe2b873463803f721d98
Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
2024-03-23 17:19:45 +0000

    Formatting changes

--- flake.nix
+++ flake.nix
@@ -22,18 +22,18 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nur = {
-      url = "github:nix-community/NUR/master";
-    };
+    nur = { url = "github:nix-community/NUR/master"; };
   };
 
-  outputs = { self, nixpkgs, darwin, emacs-overlay, nur, home-manager, deploy-rs, ... }@inputs:
+  outputs = { self, nixpkgs, darwin, emacs-overlay, nur, home-manager, deploy-rs
+    , ... }@inputs:
     let
       machost = { system, host, username ? "mryall" }:
         darwin.lib.darwinSystem {
           inherit system;
           modules = [
-            ./hosts/macos/${host}.nix { nixpkgs.overlays = [ emacs-overlay.overlay ]; }
+            ./hosts/macos/${host}.nix
+            { nixpkgs.overlays = [ emacs-overlay.overlay ]; }
             home-manager.darwinModules.home-manager
             ./home-manager/macos/${host}.nix
           ];
@@ -62,7 +62,11 @@
             inherit username;
             inherit inputs;
             inherit nixpkgs;
-            pkgs = import nixpkgs { inherit system; overlays = [ emacs-overlay.overlay ]; config.allowUnfree = true; };
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ emacs-overlay.overlay ];
+              config.allowUnfree = true;
+            };
             nur = import nur {
               pkgs = import nixpkgs { inherit system; };
               nurpkgs = import nixpkgs { inherit system; };
@@ -74,71 +78,79 @@
           system = "aarch64-linux";
           modules = [
             "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-            (
-              let
-                pkgs = nixpkgs.legacyPackages.aarch64-linux;
-              in
-              {
-                system.stateVersion = "22.05";
-                nixpkgs = {
-                  buildPlatform.system = "x86_64-linux";
-                  hostPlatform.system = "aarch64-linux";
-                };
-                networking = {
-                  wireless.enable = true;
-                };
-                console = {
-                  earlySetup = true;
-                  packages = with pkgs; [ terminus_font ];
-                  font = nixpkgs.lib.mkDefault "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
-                };
-                users.users.mryall = {
-                  isNormalUser = true;
-                  home = "/home/mryall";
-                  description = "Matthew";
-                  extraGroups = [ "wheel" "networkmanager" ];
-                  hashedPassword = "";
-                };
-              }
-            )
+            (let pkgs = nixpkgs.legacyPackages.aarch64-linux;
+            in {
+              system.stateVersion = "22.05";
+              nixpkgs = {
+                buildPlatform.system = "x86_64-linux";
+                hostPlatform.system = "aarch64-linux";
+              };
+              networking = { wireless.enable = true; };
+              console = {
+                earlySetup = true;
+                packages = with pkgs; [ terminus_font ];
+                font = nixpkgs.lib.mkDefault
+                  "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
+              };
+              users.users.mryall = {
+                isNormalUser = true;
+                home = "/home/mryall";
+                description = "Matthew";
+                extraGroups = [ "wheel" "networkmanager" ];
+                hashedPassword = "";
+              };
+            })
           ];
         };
-    in
-    {
-      devShell =
-        let
-          pkglist = pkgs: [
-            pkgs.nixfmt
-            pkgs.nixpkgs-fmt
-            pkgs.rnix-lsp
-          ];
-        in
-        inputs.nixpkgs.lib.listToAttrs (map
-          (system: {
-            name = system;
-            value = nixpkgs.legacyPackages.${system}.mkShell {
-              nativeBuildInputs = pkglist nixpkgs.legacyPackages.${system};
-            };
-          }) [ "x86_64-darwin" "x86_64-linux" "aarch64-darwin" ]);
+    in {
+      devShell = let pkglist = pkgs: [ pkgs.nixfmt pkgs.nixpkgs-fmt pkgs.nil ];
+      in inputs.nixpkgs.lib.listToAttrs (map (system: {
+        name = system;
+        value = nixpkgs.legacyPackages.${system}.mkShell {
+          nativeBuildInputs = pkglist nixpkgs.legacyPackages.${system};
+        };
+      }) [ "x86_64-darwin" "x86_64-linux" "aarch64-darwin" ]);
       nixosConfigurations = {
-        sanchez = nixoshost { system = "x86_64-linux"; host = "sanchez"; };
-        rossi = nixoshost { system = "x86_64-linux"; host = "rossi"; };
+        sanchez = nixoshost {
+          system = "x86_64-linux";
+          host = "sanchez";
+        };
+        rossi = nixoshost {
+          system = "x86_64-linux";
+          host = "rossi";
+        };
         rpi = rpihost { host = "test"; };
       };
       darwinConfigurations = {
-        socrates = machost { system = "x86_64-darwin"; host = "socrates"; };
-        careca = machost { system = "aarch64-darwin"; host = "careca"; username = "matthew.ryall"; };
-        platini = machost { system = "x86_64-darwin"; host = "platini"; };
-        robson = machost { system = "x86_64-darwin"; host = "robson"; };
+        socrates = machost {
+          system = "x86_64-darwin";
+          host = "socrates";
+        };
+        careca = machost {
+          system = "aarch64-darwin";
+          host = "careca";
+          username = "matthew.ryall";
+        };
+        platini = machost {
+          system = "x86_64-darwin";
+          host = "platini";
+        };
+        robson = machost {
+          system = "x86_64-darwin";
+          host = "robson";
+        };
       };
 
       deploy.nodes.host.profiles.system = {
         user = "root";
         hostname = "129.151.93.130";
-        path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.host;
+        path = deploy-rs.lib.x86_64-linux.activate.nixos
+          self.nixosConfigurations.host;
       };
 
       # This is highly advised, and will prevent many possible mistakes
-      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+      checks = builtins.mapAttrs
+        (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
     };
+
 }
